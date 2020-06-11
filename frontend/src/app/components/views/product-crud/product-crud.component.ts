8b6f386d5955533b73fcabe5a184a2ded79a004a
Kelvis Borges <kelvisbcc@gmail.com>
2020-06-11 18:35:09 -0300

    Frontend Finished

---
+++ frontend/src/app/components/views/product-crud/product-crud.component.ts
@@ -0,0 +1,26 @@
+import { HeaderService } from "./../../template/header/header.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-product-crud",
+  templateUrl: "./product-crud.component.html",
+  styleUrls: ["./product-crud.component.css"],
+})
+export class ProductCrudComponent implements OnInit {
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de Produtos",
+      icon: "storefront",
+      routeUrl: "/products",
+    };
+  }
+
+  ngOnInit(): void {}
+
+  navigateToProductCreate() {
+    console.log("Navegando...");
+
+    this.router.navigate(["/products/create"]);
+  }
+}
